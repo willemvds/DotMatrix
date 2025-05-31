6f2c78f65c96c231eb0a0b48e5d41aa1c1a83077
Willem van der Schyff <willemvds@gmail.com>
2025-05-31 13:31:21 +0200

    feat: Only want virtual line diagnostic info for current line.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,13 +6,13 @@ vim.wo.number = true
 
 vim.diagnostic.config({
 	-- Use the default configuration
-	virtual_lines = true,
+	-- virtual_lines = true,
 
 	-- Alternatively, customize specific options
-	-- virtual_lines = {
-	--  -- Only show virtual line diagnostics for the current cursor line
-	--  current_line = true,
-	-- },
+	virtual_lines = {
+		-- Only show virtual line diagnostics for the current cursor line
+		current_line = true,
+	},
 })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
