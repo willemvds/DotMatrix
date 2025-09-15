707e9e5c986b15a1edfe6e1a40f23bcd008ac5be
Willem van der Schyff <willemvds@gmail.com>
2025-09-15 15:52:53 +0200

    feat: Add Alacritty+neovim as default text editor.

--- arch-install.sh
+++ arch-install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+# Install common tools.
 pacman -S bat dog duf fd fish fzf ripgrep lsd
 pacman -S ttf-sourcecodepro-nerd neovim
+
+# Make Alacritty+neovim default text editor.
+xdg-mime default Alacritty-nvim.desktop text/plain
 
