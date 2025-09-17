5b7a7574e5c0790505a974aba1cfad3b29dfa713
Willem van der Schyff <willemvds@gmail.com>
2025-09-17 16:08:19 +0200

    feat: Add SuperHTML LSP. Very nice!

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -163,6 +163,18 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
+# https://github.com/kristoff-it/superhtml
+vim.api.nvim_create_autocmd("Filetype", {
+	pattern = { "html", "shtml", "htm" },
+	callback = function()
+		vim.lsp.start({
+			name = "superhtml",
+			cmd = { "superhtml", "lsp" },
+			root_dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1]),
+		})
+	end,
+})
+
 vim.lsp.enable({
 	"gopls",
 	"rust_analyzer",
