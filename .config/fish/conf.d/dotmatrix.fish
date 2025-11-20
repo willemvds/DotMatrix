875ea6d57ea671f28478003c86060a5de826aaef
Willem van der Schyff <willemvds@gmail.com>
2025-11-20 16:52:51 +0200

    feat: Add theme for 'bat'.

--- .config/fish/conf.d/dotmatrix.fish
+++ .config/fish/conf.d/dotmatrix.fish
@@ -1,4 +1,5 @@
-set EDITOR "/usr/bin/nvim"
+set -x EDITOR "/usr/bin/nvim"
+set -x BAT_THEME "gruvbox-dark"
 
 alias lsdl="lsd -lA --date '+%F %T' --group-dirs 'first' --size 'short' --blocks 'permission,user,group,date,size,name'"
 
