348e689f09ce3407029d6e96551ef3e6aefc450e
Willem van der Schyff <willemvds@gmail.com>
2026-07-20 16:03:04 +0200

    feat: Add '-a' (eza) to `ll` alias.

--- .config/fish/conf.d/dotmatrix.fish
+++ .config/fish/conf.d/dotmatrix.fish
@@ -2,5 +2,5 @@ set -x EDITOR "/usr/bin/nvim"
 set -x BAT_THEME "gruvbox-dark"
 
 alias lsdl="lsd -lA --date '+%F %T' --group-dirs 'first' --size 'short' --blocks 'permission,user,group,date,size,name'"
-alias ll="eza -l"
+alias ll="eza -la"
 
