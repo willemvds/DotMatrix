91f21784f207c736e0ae89049a093bee0c1f27e8
Willem van der Schyff <willemvds@gmail.com>
2026-07-20 06:04:18 +0200

    feat: Keep arch-install up to date.

--- arch-install.sh
+++ arch-install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Install common tools.
-pacman -S bat dog duf fd fish fzf ripgrep lsd
-pacman -S ttf-sourcecodepro-nerd neovim
+pacman -S bat eza duf fd fish fzf ripgrep lsd
+pacman -S ttf-sourcecodepro-nerd neovim alacritty ghostty
 
 # Make Alacritty+neovim default text editor.
 xdg-mime default Alacritty-nvim.desktop text/plain
